6f23e73914e64b507e2779a8a630e6a964abda00
Winifred Frierson <winifred.dykes@hotmail.com>
2019-09-30 05:49:31 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,38 @@
 def first_challenge
    epic_tragedy = {}
+      :montague => {
+        :patriarch => 
+          {:name = "Lord Montague", 
+          :age => "53"
+        },
+        :matriarch => 
+          {:name => "Lady Montague", 
+          :age => "54"
+        },
+        :hero => 
+          {:name => "Romeo", 
+          :age => "15"
+        },
+        :hero_friends => [
+            :name =>
+      "Benvolio",
+                :age => "17",
+                :attitude =>
+      "worried"
+            },
+            {
+              :name =>
+      "Mercutio",
+              :age => "18",
+              :attitude => "hot-
+      headed"
+            }
+              
+          ]
+        },
+        
+        
+      :capulet => {}
+  }
+  Montague"
 end
